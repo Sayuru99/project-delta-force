9865ddf5b28a01892ac840cfad105e5faca1748d
sayuru99 <saurudealwis@ymail.com>
2024-12-05 12:26:44 +0530

    fix tailwind css issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,18 +3,14 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 
 const Home = () => (
-  <main className="p-8">
-    <h1 className="text-3xl font-semibold mb-4">Welcome to BPN</h1>
-    <p className="text-lg mb-6">
-      This is a sample website built using Vite, React, and Tailwind.
-    </p>
-    <h1>Test Tharusha</h1>
-    <h1>ses ses</h1>
+  <main className="flex items-center justify-center min-h-screen flex-col">
+    <h1 className="text-3xl font-bold mb-6">Hello world!</h1>
+
     <LazyLoadImage
       alt="Sample Image"
       src="https://bpn.designsfrontier.com/wp-content/uploads/2024/11/news.png"
       effect="blur"
-      width="100%"
+      className="w-full max-w-md"
     />
   </main>
 );
